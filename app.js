fbe8232dbdf07ea1935ed00d1a5fddc3f8341634
Clara <claraadora@gmail.com>
2020-08-25 23:19:19 +0800

    Change to express.js

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http = require("http");
+const express = require("express");
 
-const routes = require("./routes");
+const app = express();
 
-const server = http.createServer(routes); // start an event loop that keeps on running as long its still registered
+app.use((req, res, next) => {
+    console.log("In the middleware");
+    next(); // Allows the request to continue to the next middleware
+});
+
+app.use((req, res, next) => {
+    console.log("In another middleware");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
