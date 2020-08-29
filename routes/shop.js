e3c059063edd86b2a2da31495a6e57eb8a4082c7
Clara <claraadora@gmail.com>
2020-08-30 03:27:19 +0800

    Add error page

--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.send(<h1>Hello!</h1>);
+    res.send("<h1>Hello!</h1>");
 });
 module.exports = router;
